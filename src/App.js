d67482268d9d1b7df37a9ed9958165953cd0371d
Bob <rbsavary88@gmail.com>
2023-04-13 17:20:16 -0400

    Fully functional crud app with some styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { useState, useEffect  } from 'react'
 import axios from 'axios'
 import Add from './components/Add'
 import Edit from './components/Edit'
+import './App.css'
+import ReactModal from 'react-modal'
 
 const App = () => {
 
   let [tasks, setTasks] = useState([])
+  // const [showAddModal, setShowAddModal] = useState(false)
 
-const getTasks = () =>{ 
+const getTasks = () => { 
   axios.get('http://127.0.0.1:8000/api/tasks')
   .then(
     (response) => setTasks(response.data), 
@@ -25,7 +28,7 @@ const handleCreate = (addTask) => {
   }
 
   const handleDelete = (event) => {
-    axios.delete('http://127.0.0.1:8000/api/task/' + event.target.value)
+    axios.delete('http://127.0.0.1:8000/api/tasks/' + event.target.value)
       .then((response) => {
         getTasks()
       })
@@ -39,20 +42,27 @@ const handleCreate = (addTask) => {
       })
   }
       
-useEffect(() =>{
+useEffect(() => {
   getTasks()
 }, [])
 
   return (
     <>
-      <div className='container'>
-        <h1 className='app-header'>Time Box</h1>
+      <div className='container mb-3'>
+        <h1 className='text-center'>Time Box</h1>
         <Add handleCreate={handleCreate} />
+        {/* <button className="btn btn-secondary btn-lg" onClick={() => setShowAddModal(true)}>Add</button> */}
         {tasks.map((task) => {
           return (
-            <div className='' key={task.id}>
-              <Edit tasks={tasks} handleUpdate={handleUpdate} />
-              <button onClick={handleDelete} value={task.id}>Delete</button>
+            <div className='card mb-3' key={task.id}>
+              <Edit handleUpdate={handleUpdate} task={task}/>
+                <h4 className='card-header'>{task.name}</h4>
+                <div className='card-body'>
+                  <h5 className=''>{task.time_to_complete} minutes to complete</h5>
+                  <h6 className=''>Notes: {task.notes}</h6>
+                  <h5 className=''>Priority: { task.priority }</h5>
+                <button className="btn btn-danger" onClick={handleDelete} value={task.id}>Delete</button>
+              </div>
             </div>
           )
         })}
--- src/components/Add.js
+++ src/components/Add.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const Add = (props) => {
-    let emptyTask = { name: '', notes: '', time_to_complete: '', priority: '' }
+    let emptyTask = { name: '', notes: '', time_to_complete: '', priority: 'DO' }
     const [task, setTask] = useState(emptyTask)
 
     const handleChange = (event) => {
@@ -10,52 +10,51 @@ const Add = (props) => {
     const handleSubmit = (event) => {
         event.preventDefault()
         props.handleCreate(task)
+        setTask(emptyTask)
+        // props.setShowAddModal(false)
     }
 
     return (
         <>
-        <div className='container'>
+        <div className='mb-3'>
             <div className="add-form">
                 <h2 className='app-header'>Add Task</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="name">Task: </label>
-                        <input className="form-control" type="text" name="name" value={task.name} onChange={handleChange}/>
+                <form className="row g-3" onSubmit={handleSubmit}>
+                    <div className="col-md-10">
+                        <label className="form-label" htmlFor="name">Task</label>
+                        <input className="form-control" type="text" name="name" required value={task.name} onChange={handleChange}/>
                     </div>
-                        <br/>
-                        <br/>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="notes">Notes: </label>
+                    <div className="col-md-2">
+                        <label className="form-label" htmlFor="time_to_complete">Estimated Time To Complete</label>
+                        <input className="form-control" name="time_to_complete" required type="number" id="timeInput" step="5" min="0" value={task.time_to_complete} onChange={handleChange}/>
+                    </div>
+                    <div className="col-12  mb-3">
+                        <label className="form-label" htmlFor="notes">Notes</label>
                         <textarea className="form-control" type="text" rows="3" name="notes" value={task.notes} onChange={handleChange}/>
                     </div>
-                        <br/>
-                        <br/>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="time_to_complete">Estimated Time To Complete: </label>
-                        <input className="form-control" name="time_to_complete" type="number" id="timeInput" step="5" min="0" value={task.time_to_complete} onChange={handleChange}/>
-                    </div>
-                        <br/>
-                        <br/>
-                    <div className="mb-3">
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" id="priority_urgent" name="priority" value="UR" onChange={handleChange}/>
-                            <label className="form-check-label" for="priority_urgent">Urgent</label>
+                    <fieldset className="row mb-3">
+                        <legend className="col-form-label col-sm-2 pt-0">Priority</legend>
+                        <div class="col-sm-10">
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" id="priority_urgent" name="priority" value="UR" onChange={handleChange}/>
+                                <label className="form-check-label" htmlFor="priority_urgent">Urgent</label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" id="priority_do" name="priority" value="DO" checked onChange={handleChange} />
+                                <label className="form-check-label" htmlFor="priority_do">Do</label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" id="priority_defer" name="priority" value="DF" onChange={handleChange}/>
+                                <label className="form-check-label" htmlFor="priority_defer">Defer</label>   
+                            </div>
                         </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" id="priority_do" name="priority" value="DO" checked onChange={handleChange} />
-                            <label className="form-check-label" for="priority_do">Do</label>
-                        </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" id="priority_defer" name="priority" value="DF" onChange={handleChange}/>
-                            <label className="form-check-label" for="priority_defer">Defer</label>   
-                        </div>
-                    </div>
-                    <input
-                    className="btn btn-secondary btn-lg"
+                    </fieldset>
+                </form>
+                <input
+                    className="btn btn-primary"
                     type="submit"
                     value="Submit"/>
-                </form>
-            </div>
+            </div>    
         </div>
         </>
     )
